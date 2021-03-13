4837ab8cc0fc36cecf35c326bf2a9aef24892f37
Philosopheya <78369694+Philosopheya@users.noreply.github.com>
2021-03-13 21:21:40 +0300

    fix: change solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,19 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  let count = 1;
-  if (Array.isArray(arr) == false) {
-    return 1;
-  } else {
-    arr.forEach(e => {
+  calculateDepth(arr) {
+    let count = 1;
+    let depth = 0;
+    arr.forEach((e) => {
       if (Array.isArray(e)) {
-        let depth = this.calculateDepth(element) + 1;
-        if (count < depth) {
-          count = depth;
-        }
+        count = this.calculateDepth(e);
+        if (depth < count) depth = count;
       }
     });
-    return max;
+    return depth + 1;
   }
-};
 };
