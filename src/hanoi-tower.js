1ea85c8be6a276095bc3b6b5d04de3c7bab02b5a
Philosopheya <78369694+Philosopheya@users.noreply.github.com>
2021-03-13 20:12:00 +0300

    feat: add missing 's'

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = (2 ** diskNumber) - 1;
+  let turns = (2 ** disksNumber) - 1;
   let seconds = Math.floor(((2 ** disksNumber) - 1) / (turnsSpeed / 3600))
   return {turns, seconds};
 };
