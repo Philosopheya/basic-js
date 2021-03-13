89fadf0d0a3430aac935527c3f43577f84b4da62
Philosopheya <78369694+Philosopheya@users.noreply.github.com>
2021-03-13 20:59:10 +0300

    init: add solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  let count = 1;
+  if (Array.isArray(arr) == false) {
+    return 1;
+  } else {
+    arr.forEach(e => {
+      if (Array.isArray(e)) {
+        let depth = this.calculateDepth(element) + 1;
+        if (count < depth) {
+          count = depth;
+        }
+      }
+    });
+    return max;
   }
+};
 };
